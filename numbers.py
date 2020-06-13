387d62b7bc679cd3defe96597d90d6611a3b4aaa
Sunitha Ramu <sunitharamu@Sunithas-MacBook-Air.local>
2020-06-13 20:04:23 +0530

    Additional Python codes added

---
+++ numbers.py
@@ -0,0 +1,17 @@
+'''
+To write a python code to learn operators and its functions - Sunitha
+'''
+
+for n in range(1,21):
+    if n % 2==0:
+        print('Even Number:',n)
+    else:
+        print('Odd Number:',n)
+
+print("Thank you, the series of number from 1 to 21 is complete.")
+
+'''To try and write somethhing related to random integers'''
+
+import random
+
+print(random.randint(1,60))
---
+++ typecast.py
@@ -0,0 +1,23 @@
+'''typecasting process - Sunitha'''
+
+x='100'
+y='-90'
+
+z=int(x)+int(y)
+print (z)
+
+z1=x+y
+print(z1)
+
+fl=float(x)+float(y)
+print(fl)
+
+list=[1,2,3,'a','b','c']
+list.extend(['e','f'])
+print(list)
+list.append(['e','f'])
+print(list)
+list.append([2,3,4])
+print(list)
+list.append(1)
+print (list)
